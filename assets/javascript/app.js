8eab535afe1a32516371b54ddffc8cc6aaa48a87
Kostiantyn <kostya20022210@gmail.com>
2025-01-22 06:11:46 +0200

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+let userInput = prompt("Enter number (0-7)");
+
+switch (userInput) {
+    case '0':
+        console.log('---');
+        break;
+    case '1':
+        console.log('--x');
+        break;
+    case '2':
+        console.log('-w-');
+        break;
+    case '3':
+        console.log('-WX');
+        break;
+    case '4':
+        console.log('r--');
+        break;
+    case '5':
+        console.log('r-x');
+        break;
+    case '6':
+        console.log('RW-');
+        break;
+    case '7':
+        console.log('RWX');
+        break;
+    default:
+        console.log('Incorrect value!');
+        break;
+}
