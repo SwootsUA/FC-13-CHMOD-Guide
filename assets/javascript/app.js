396f5eb575596dc547f34d31771a7dd5ae395806
Kostiantyn <kostya20022210@gmail.com>
2025-01-22 08:59:15 +0200

    Fix: convert user input to number for switch case evaluation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,28 @@
-let userInput = prompt('Enter number (0-7)');
+let userInput = Number(prompt('Enter number (0-7)'));
 
 switch (userInput) {
-    case '0':
+    case 0:
         console.log('---');
         break;
-    case '1':
+    case 1:
         console.log('--x');
         break;
-    case '2':
+    case 2:
         console.log('-w-');
         break;
-    case '3':
+    case 3:
         console.log('-WX');
         break;
-    case '4':
+    case 4:
         console.log('r--');
         break;
-    case '5':
+    case 5:
         console.log('r-x');
         break;
-    case '6':
+    case 6:
         console.log('RW-');
         break;
-    case '7':
+    case 7:
         console.log('RWX');
         break;
     default:
